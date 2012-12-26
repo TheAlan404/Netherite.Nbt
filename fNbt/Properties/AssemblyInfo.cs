8b8c0b7caa852356034de75e47c20aa785e78992
fragmer <fragmer@gmail.com>
2012-12-25 22:39:13 -0700

    Updated assembly metadata to remove the last references to LibNbt/LibNbt2012

--- fNbt/Properties/AssemblyInfo.cs
+++ fNbt/Properties/AssemblyInfo.cs
@@ -1,23 +1,39 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Resources;
 
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
 [assembly: AssemblyTitle( "fNbt" )]
-[assembly: AssemblyDescription("A library to read and write NBT files.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany( "github.com/fragmer/fnbt" )]
+[assembly: AssemblyDescription( "A library to read and write NBT files." )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "github.com/fragmer/fNbt" )]
 [assembly: AssemblyProduct( "fNbt" )]
-[assembly: AssemblyCopyright( "fNbt - 2012-2013 Matvei Stefarov; LibNbt - 2010 Erik Davidson," )]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright( "2012-2013 Matvei Stefarov" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
 
-[assembly: ComVisible(false)]
-[assembly: Guid("9253db1f-f1d4-45aa-a277-4f3ba635d651")]
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible( false )]
 
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid( "9253db1f-f1d4-45aa-a277-4f3ba635d651" )]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion( "0.5.0.0" )]
 [assembly: AssemblyFileVersion( "0.5.0.0" )]
 
-// Allow the unit tests to see internal classes
-[assembly: InternalsVisibleTo("LibNbt.Test")]
+// etc
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
