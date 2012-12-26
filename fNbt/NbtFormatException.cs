b2cf45f91b810164c85315a09e58a31480eff888
fragmer <fragmer@gmail.com>
2012-12-25 21:06:04 -0700

    Rebranded, LibNbt2012 -> fNbt

---
+++ fNbt/NbtFormatException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace fNbt {
+    /// <summary> Exception thrown when a format violation is detected while parsing or saving an NBT file. </summary>
+    [Serializable]
+    public sealed class NbtFormatException : Exception {
+        internal NbtFormatException( string message )
+            : base( message ) {}
+    }
+}
---
+++ fNbt/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+[assembly: AssemblyTitle( "fNbt" )]
+[assembly: AssemblyDescription("A library to read and write NBT files.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany( "github.com/fragmer/fnbt" )]
+[assembly: AssemblyProduct( "fNbt" )]
+[assembly: AssemblyCopyright( "fNbt - 2012-2013 Matvei Stefarov; LibNbt - 2010 Erik Davidson," )]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("9253db1f-f1d4-45aa-a277-4f3ba635d651")]
+
+[assembly: AssemblyVersion( "0.5.0.0" )]
+[assembly: AssemblyFileVersion( "0.5.0.0" )]
+
+// Allow the unit tests to see internal classes
+[assembly: InternalsVisibleTo("LibNbt.Test")]
+[assembly: NeutralResourcesLanguageAttribute( "en-US" )]
