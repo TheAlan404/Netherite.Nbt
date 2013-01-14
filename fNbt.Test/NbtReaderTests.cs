fdf1d1d7d2deb854c977d614788f3ecc7fa00092
fragmer <fragmer@gmail.com>
2013-01-14 10:18:18 -0800

    Expanded unit test coverage for NbtReader

--- fNbt.Test/NbtReaderTests.cs
+++ fNbt.Test/NbtReaderTests.cs
@@ -6,7 +6,7 @@ namespace fNbt.Test {
     [TestFixture]
     public sealed class NbtReaderTests {
         [Test]
-        public void ReadBigFileUncompressed() {
+        public void PrintBigFileUncompressed() {
             using( FileStream fs = File.OpenRead( "TestFiles/bigtest.nbt" ) ) {
                 NbtReader reader = new NbtReader( fs );
                 while( reader.ReadToFollowing() ) {
@@ -41,60 +41,64 @@ namespace fNbt.Test {
         }
 
 
-        [Test]
-        public void ReadToSiblingTest() {
+        static Stream MakeTest() {
             NbtCompound root = new NbtCompound( "root" ) {
-                new NbtInt("first"),
-                new NbtInt("second"),
-                new NbtCompound("third-comp"){
-                    new NbtInt("sub1"),
-                    new NbtInt("sub2"),
-                    new NbtInt("sub3")
+                new NbtInt( "first" ),
+                new NbtInt( "second" ),
+                new NbtCompound( "third-comp" ) {
+                    new NbtInt( "sub1" ),
+                    new NbtInt( "sub2" ),
+                    new NbtInt( "sub3" )
                 },
-                new NbtInt("fourth")
+                new NbtInt( "fourth" )
             };
             byte[] testData = new NbtFile( root ).SaveToBuffer( NbtCompression.None );
-            using( MemoryStream ms = new MemoryStream( testData ) ) {
-                NbtReader reader = new NbtReader( ms );
-                Assert.IsTrue( reader.ReadToFollowing() );
-                Assert.AreEqual( reader.TagName, "root" );
-                Assert.IsTrue( reader.ReadToFollowing() );
-                Assert.AreEqual( reader.TagName, "first" );
-                Assert.IsTrue( reader.ReadToNextSibling( "third-comp" ) );
-                Assert.AreEqual( reader.TagName, "third-comp" );
-                Assert.IsTrue( reader.ReadToNextSibling() );
-                Assert.AreEqual( reader.TagName, "fourth" );
-                Assert.IsFalse( reader.ReadToNextSibling() );
-            }
+            return new MemoryStream( testData );
+        }
+
+
+        [Test]
+        public void ReadToSiblingTest() {
+            NbtReader reader = new NbtReader( MakeTest() );
+            Assert.IsTrue( reader.ReadToFollowing() );
+            Assert.AreEqual( reader.TagName, "root" );
+            Assert.IsTrue( reader.ReadToFollowing() );
+            Assert.AreEqual( reader.TagName, "first" );
+            Assert.IsTrue( reader.ReadToNextSibling( "third-comp" ) );
+            Assert.AreEqual( reader.TagName, "third-comp" );
+            Assert.IsTrue( reader.ReadToNextSibling() );
+            Assert.AreEqual( reader.TagName, "fourth" );
+            Assert.IsFalse( reader.ReadToNextSibling() );
         }
 
 
         [Test]
         public void ReadToDescendantTest() {
-            NbtCompound root = new NbtCompound( "root" ) {
-                new NbtInt("first"),
-                new NbtInt("second"),
-                new NbtCompound("third-comp"){
-                    new NbtInt("sub1"),
-                    new NbtInt("sub2"),
-                    new NbtInt("sub3")
-                },
-                new NbtInt("fourth")
-            };
-            byte[] testData = new NbtFile( root ).SaveToBuffer( NbtCompression.None );
-            using( MemoryStream ms = new MemoryStream( testData ) ) {
-                NbtReader reader = new NbtReader( ms );
+            {
+                NbtReader reader = new NbtReader( MakeTest() );
                 Assert.IsTrue( reader.ReadToDescendant( "third-comp" ) );
                 Assert.AreEqual( reader.TagName, "third-comp" );
                 Assert.IsTrue( reader.ReadToDescendant( "sub3" ) );
                 Assert.AreEqual( reader.TagName, "sub3" );
                 Assert.IsFalse( reader.ReadToDescendant( "derp" ) );
+                Assert.AreEqual( reader.TagName, "fourth" );
             }
-            using( MemoryStream ms = new MemoryStream( testData ) ) {
-                NbtReader reader = new NbtReader( ms );
+            {
+                NbtReader reader = new NbtReader( MakeTest() );
                 Assert.IsTrue( reader.ReadToDescendant( "sub2" ) );
                 Assert.AreEqual( reader.TagName, "sub2" );
             }
+        }
+
+
+        [Test]
+        public void SkipTest() {
+            NbtReader reader = new NbtReader( MakeTest() );
+            reader.ReadToFollowing(); // at root
+            reader.ReadToFollowing(); // at first
+            Assert.AreEqual( reader.TagName, "first" );
+            Assert.AreEqual( reader.Skip(), 6 );
+            Assert.IsFalse( reader.ReadToFollowing() );
         }
     }
 }
