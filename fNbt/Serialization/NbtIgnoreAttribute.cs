143c04154979f50c1abfcec111940f2e7d2bf9cc
fragmer <fragmer@gmail.com>
2013-03-17 17:34:59 -0700

    Axed NbtSerializer subproject. It wasn't as effective or efficient as expected.

--- fNbt/Serialization/NbtIgnoreAttribute.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace fNbt.Serialization {
-    /// <summary> Presence of this attribute instructs NbtSerializer to always
-    /// skip an object's property when serializing/deserializing. </summary>
-    [AttributeUsage( AttributeTargets.Property, AllowMultiple = false )]
-    public class NbtIgnoreAttribute : Attribute {}
-}
