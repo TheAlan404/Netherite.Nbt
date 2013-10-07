d541dc91cb6a118add3b060e41edc6540e1ffabf
fragmer <fragmer@gmail.com>
2013-10-06 18:14:30 -0600

    Eliminated the possibility of adding a Compound tag to itself. It's still theoretically possible to make an inheritance loop (add child tag to parent tag), but checking for that would be rather expensive.

--- fNbt/Tags/NbtCompound.cs
+++ fNbt/Tags/NbtCompound.cs
@@ -71,6 +71,8 @@ namespace fNbt {
                     throw new ArgumentException( "Given tag name must match tag's actual name." );
                 } else if( value.Parent != null ) {
                     throw new ArgumentException( "A tag may only be added to one compound/list at a time." );
+                }else if( value == this ) {
+                    throw new ArgumentException( "Cannot add tag to itself" );
                 }
                 tags[tagName] = value;
                 value.Parent = this;
@@ -553,17 +555,6 @@ namespace fNbt {
         }
 
         #endregion
-
-
-        // Hiding NbtTag[int]:NbtTag
-        new NbtTag this[ int tagIndex ] {
-            get {
-                throw new InvalidOperationException( "Integer indexers only work on NbtList tags." );
-            }
-            set {
-                throw new InvalidOperationException( "Integer indexers only work on NbtList tags." );
-            }
-        }
 
 
         internal override void PrettyPrint( StringBuilder sb, string indentString, int indentLevel ) {
