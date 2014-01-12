4b7dfe1a84ddc06566ab6419513ed586a21ee4f5
fragmer <me@matvei.org>
2014-01-12 03:49:26 -0700

    Fixed an edge case in ByteCountingStream that affected reporting accuracy in streams that implement Read/Write by calling ReadByte/WriteByte.

--- fNbt/ByteCountingStream.cs
+++ fNbt/ByteCountingStream.cs
@@ -3,12 +3,15 @@ using System.IO;
 using JetBrains.Annotations;
 
 namespace fNbt {
+    // Class used to count bytes read-from/written-to non-seekable streams.
     internal class ByteCountingStream : Stream {
         readonly Stream baseStream;
 
         // These are necessary to avoid counting bytes twice if ReadByte/WriteByte call Read/Write internally.
-        bool readingOneByte;
-        bool writingOneByte;
+        bool readingOneByte, writingOneByte;
+
+        // These are necessary to avoid counting bytes twice if Read/Write call ReadByte/WriteByte internally.
+        bool readingManyBytes, writingManyBytes;
 
 
         public ByteCountingStream([NotNull] Stream stream) {
@@ -33,14 +36,18 @@ namespace fNbt {
 
 
         public override int Read(byte[] buffer, int offset, int count) {
+            readingManyBytes = true;
             int bytesActuallyRead = baseStream.Read(buffer, offset, count);
-            if(!readingOneByte) BytesRead += bytesActuallyRead;
+            readingManyBytes = false;
+            if (!readingOneByte) BytesRead += bytesActuallyRead;
             return bytesActuallyRead;
         }
 
 
         public override void Write(byte[] buffer, int offset, int count) {
+            writingManyBytes = true;
             baseStream.Write(buffer, offset, count);
+            writingManyBytes = false;
             if (!writingOneByte) BytesWritten += count;
         }
 
@@ -49,16 +56,16 @@ namespace fNbt {
             readingOneByte = true;
             int value = base.ReadByte();
             readingOneByte = false;
-            if (value >= 0) BytesRead++;
+            if (value >= 0 && !readingManyBytes) BytesRead++;
             return value;
         }
 
-        
+
         public override void WriteByte(byte value) {
             writingOneByte = true;
             base.WriteByte(value);
             writingOneByte = false;
-            BytesWritten++;
+            if (!writingManyBytes) BytesWritten++;
         }
 
 
