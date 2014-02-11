ebb2b4d60866ee4d819922fa9826a6f31702aa24
fragmer <me@matvei.org>
2014-02-11 10:12:41 -0700

    Added a simple unit test for NbtCompiler

---
+++ fNbt.Test/NbtCompilerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using fNbt.Serialization;
+using NUnit.Framework;
+
+namespace fNbt.Test {
+    [TestFixture]
+    internal class NbtCompilerTests {
+        [Test]
+        public void ValueTest() {
+            var serializer = NbtCompiler.GetSerializer<TestFiles.ValueTestClass>();
+            TestFiles.ValueTestClass testObject = TestFiles.MakeValueTestObject();
+            NbtCompound serializedTag = serializer("root", testObject);
+            TestFiles.AssertValueTest(new NbtFile(serializedTag));
+        }
+    }
+}
